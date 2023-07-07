9920a89683c7e12d66bf748d025bac121db10870
Akash NP <akashnp1998@gmail.com>
2023-07-07 20:43:38 +0400

    fix: fix cleaning of llm flashcard response

--- server/utils/functions.py
+++ server/utils/functions.py
@@ -5,5 +5,4 @@ def clean_flashcard_response(raw_response: str) -> list:
     converts the raw string response of list of dictionaires
     created by openai into actual list of dictionaries
     """
-    response = raw_response.replace("'", "\"")
-    return json.loads(response)
+    return json.loads(r"{}".format(raw_response))
