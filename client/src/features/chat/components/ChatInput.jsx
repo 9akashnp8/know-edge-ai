2c47f045ccec165dda0f1947a97b87c04f76e6a7
Akash NP <akashnp1998@gmail.com>
2024-06-30 14:58:54 +0400

    chore!: refactor chat stream handling
    - use localStorage to store messages, currently only a basic implementa
    -tion. TODO: differentiate based on document, sync messages to server
    in web worker
    - move API call to ChatInput, isolates any state updates and their
    resultant re-renders to the component itself.

--- client/src/features/chat/components/ChatInput.jsx
+++ client/src/features/chat/components/ChatInput.jsx
@@ -1,6 +1,5 @@
 // Hooks and other functions
-import { setMessage } from "../chatSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { fetchEventSource } from "@microsoft/fetch-event-source";
 
 // Material UI Components
 import TextField from "@mui/material/TextField"
@@ -8,10 +7,51 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 // Internal UI Components
 import AskButton from "./AskButton";
+import { useState } from "react";
 
-export default function ChatInput({ loading, handleClickSendMessage }) {
-    const dispatch = useDispatch();
-    const chatMessage = useSelector((state) => state.chatMessage)
+export default function ChatInput({ setChatResponse }) {
+    const [ chatMessage, setChatMessage ] = useState()
+    const [ loading, setLoading ] = useState()
+    const messageHistory = JSON.parse(localStorage.getItem("messageHistory") || "[]")
+
+    function handleClickSendMessage (question) {
+        const controller = new AbortController()
+        const { signal } =  controller
+        localStorage.setItem(
+            "messageHistory",
+            JSON.stringify(
+                [...messageHistory, { user: 'user', message: question }]
+            )
+        )
+        setLoading(true)
+        fetchEventSource("http://localhost:8000/api/stream", {
+            method: "POST",
+            headers: {
+                "Accept": "text/event-stream",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ "message": question }),
+            onerror: (_) => {
+                controller.abort()
+                throw new Error()
+            },
+            onmessage: (ev) => {
+                const message = ev.data
+                if (ev.event == "message") {
+                    setChatResponse(message)
+                } else if (ev.event == "end") {
+                    const currMessageHistory = JSON.parse(localStorage.getItem("messageHistory"))
+                    const systemMessage = { user: 'system', message: message }
+                    localStorage.setItem("messageHistory", JSON.stringify([...currMessageHistory, systemMessage]))
+                }
+            },
+            onclose: () => {
+                setLoading(false)
+                controller.abort()
+            },
+            signal: signal,
+        })
+    };
 
     return (
         <TextField
@@ -21,7 +61,7 @@ export default function ChatInput({ loading, handleClickSendMessage }) {
             maxRows={4}
             sx={{ mt: 3 }}
             value={chatMessage}
-            onChange={(e) => { dispatch(setMessage(e.target.value)) }}
+            onChange={(e) => { setChatMessage(e.target.value) }}
             InputProps={{
                 endAdornment: !loading
                     ? <AskButton onClick={() => handleClickSendMessage(chatMessage)} />
